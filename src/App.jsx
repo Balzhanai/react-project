53c9a52a90b00c65bb48a8057430ec25de90e95a
Balzhanai <balzansejdaly1@gmail.com>
2024-04-25 11:20:16 +0500

    react-project

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div>
      <Header/>
      <HeroSection/>
-     <RestaurantList/>
+     
     </div>
   )
 }
--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,13 +1,25 @@
 import { useGetFoodQuery } from "../api/post";
-import RestaurantCard from "./RestaurantCard";
+import RestaurantCard from "./RestaurantCard"; 
 
 function RestaurantList() {
     const {data, restaurants, isLoading, error } = useGetFoodQuery()
+
+    if(isLoading) {
+        return(
+            <p>Идет загрузка...</p>
+        )
+    }
+
     return (
+        <div>
         {
-            restaurants.map(restaurant => <RestaurantCard
-            name={restaurant})
+             restaurants.map(restaurant => <RestaurantCard
+             key={restaurant.id}
+            name={restaurant.name}
+            categories={restaurant.categories}
+        />)
         }
+        </div>
     )
         
     
